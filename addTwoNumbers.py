ea403c610953702f56bf59362886e4ddfa1495ab
Boris <boris.vladimirov13@gmail.com>
2021-10-20 21:04:39 +0300

    addTwoNumbers.py start

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -4,5 +4,29 @@ class ListNode:
         self.val = val
         self.next = next
 
+
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+    @staticmethod
+    def reversalLinkedList(head, linkedList):
+        current = head
+        prev = None
+        next_node = None
+
+    # @staticmethod
+    # def searchHead(node) -> bool:
+    #
+    @staticmethod
+    def addTwoNumbers(l1, l2):
+        return l1, l2
+
+
+if __name__ == '__main__':
+    Solution.addTwoNumbers(l1=ListNode(val=2, next=
+                                    ListNode(val=4, next=
+                                             ListNode(val=3))),
+                           l2=ListNode(val=5, next=
+                                    ListNode(val=6, next=
+                                             ListNode(val=4))))
+
+
