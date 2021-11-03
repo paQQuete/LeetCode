9348e165a1d6940694be79827077325c6744564a
Boris <boris.vladimirov13@gmail.com>
2021-11-03 14:20:12 +0300

    Rotate Image.py - Done

--- Rotate Image.py
+++ Rotate Image.py
@@ -3,21 +3,21 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # tempMatrix = [[] for x in range(0, len(matrix))]
+        tempMatrix = [[] for x in range(0, len(matrix))]
         j = 0
         stroka = 0
         trueMatrixLen = len(matrix)
         for k in range(0, len(matrix)):
             matrix.append([])
         for i in range(0, trueMatrixLen*trueMatrixLen):
-            if i % 4 == 0 and i != 0:
+            if i % trueMatrixLen == 0 and i != 0:
                 stroka += 1
                 j = 0
 
 
 
             matrix[j+trueMatrixLen].append(matrix[stroka][j])
-            if j % 4 == 0 and j != 0:
+            if j % trueMatrixLen == 0 and j != 0:
                 j = 0
             else:
                 j += 1
@@ -28,13 +28,11 @@ class Solution:
         for i in range(0, len(matrix)):
             matrix[i].reverse()
 
+        return matrix
         # решение выше верное, но на ЛитКод при добавлении ошибка доступа по индексу, видимо есть ограничение
 
 if __name__ == '__main__':
-    matrix = [[5, 1, 9, 11],
-              [2, 4, 8, 10],
-              [13, 3, 6, 7],
-              [15, 14, 12, 16]]
+    matrix = [[1,2,3],[4,5,6],[7,8,9]]
 
     print(Solution().rotate(matrix))
     # Output: [[15, 13, 2, 5],
